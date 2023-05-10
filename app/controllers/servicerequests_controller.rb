a5b8e2d9484d85ef955b0b4e2525c2793acc7d76
Michael Callahan <mcalla242@gmail.com>
2023-05-10 15:41:44 -0600

    Add User Story 4 testing specs and features

--- app/controllers/servicerequests_controller.rb
+++ app/controllers/servicerequests_controller.rb
@@ -2,4 +2,8 @@ class ServicerequestsController < ApplicationController
   def index
     @servicerequests = Servicerequest.all
   end
+
+  def show
+    @servicerequest = Servicerequest.find(params[:id])
+  end
 end
--- spec/features/servicerequests/show_spec.rb
+++ spec/features/servicerequests/show_spec.rb
@@ -6,6 +6,17 @@ RSpec.describe "the Service Requests show page" do
     service_request_1 = Servicerequest.create!(bike_from_shop: true, estimated_cost: 200, customer_name: "Michael Callahan", bike_type: "Road")
     service_request_2 = Servicerequest.create!(bike_from_shop: false, estimated_cost: 40, customer_name: "Primoz Roglic", bike_type: "Road")
 
-    
+    visit "/servicerequests/#{service_request_2.id}"
+    save_and_open_page
+    expect(page).to have_content("Customer ID: #{service_request_2.id}")
+    expect(page).to have_content("Customer Name: #{service_request_2.customer_name}")
+    expect(page).to have_content("Bike Type: #{service_request_2.bike_type}")
+    expect(page).to have_content("Estimated Cost: #{service_request_2.estimated_cost}")
+    expect(page).to have_content("Bike from Shop: #{service_request_2.bike_from_shop}")
+
+    expect(page).to_not have_content("Customer ID: #{service_request_1.id}")
+    expect(page).to_not have_content("Customer Name: #{service_request_1.customer_name}")
+    expect(page).to_not have_content("Estimated Cost: #{service_request_1.estimated_cost}")
+    expect(page).to_not have_content("Bike from Shop: #{service_request_1.bike_from_shop}")
   end
 end
